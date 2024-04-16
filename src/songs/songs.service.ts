282f9f8ee74badc91eb1880ba4708478aff6fccf
rod.gc <rod.gc@hotmail.com>
2024-04-16 13:45:41 -0600

    Create Song Service template

--- src/songs/songs.service.ts
+++ src/songs/songs.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class SongsService {}
+export class SongsService {
+  private readonly songs = [];
+
+  create(song) {
+    // Save the song in the database
+    this.songs.push(song);
+    return this.songs;
+  }
+
+  findAll() {
+    // Retrieve songs from Database
+    return this.songs;
+  }
+}

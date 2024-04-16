8aa54b93af9c71dba02f1271f6574f21ea3a2d35
rod.gc <rod.gc@hotmail.com>
2024-04-16 14:14:12 -0600

    Create Connection constants

---
+++ src/common/constants/connection.ts
@@ -0,0 +1,11 @@
+export const connection: Connection = {
+  CONNECTION_STRING: 'CONNECTION_STRING',
+  DB: 'MYSQL',
+  DB_NAME: 'TEST',
+};
+
+export type Connection = {
+  CONNECTION_STRING: string;
+  DB: string;
+  DB_NAME: string;
+};

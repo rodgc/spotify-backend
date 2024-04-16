c3a34a33644eb138441158292157f1564b414758
rod.gc <rod.gc@hotmail.com>
2024-04-16 02:20:42 -0600

    Create template for Controllers API Calls

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+  @Post()
+  crate() {
+    return 'create a new song';
+  }
+
+  @Get()
+  findAll() {
+    return 'Find All Songs';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'fetch song on the based on id';
+  }
+
+  @Put(':id')
+  update() {
+    return 'update song on the based on id';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'delete song on the based on id';
+  }
+}

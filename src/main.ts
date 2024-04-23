d0f3832b4d9f417c399e7cd1a47d02e8bde94b6a
rod.gc <rod.gc@hotmail.com>
2024-04-23 01:43:22 -0600

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
+  // Swagger Module Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Spotify Clone')
+    .setDescription('The Spotify Clone Api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Seeding
   // const seedService = app.get(SeedService);
   // await seedService.seed();
 
